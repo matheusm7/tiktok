303e17a5e8ea66c69c0195170d102992a84dbd7d
matheus <matheusidmelo@gmail.com>
2024-01-29 15:58:18 -0300

    29/1

---
+++ src/pages/home/index.js
@@ -0,0 +1,15 @@
+import { View, StyleSheet, Text} from 'react-native'
+
+export function Home() {
+    return(
+        <View styles={styles.container}>
+            <Text>Página Home</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    }
+})
